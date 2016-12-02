7ff1853b5a192b2eccf713789f45a0690a6edddb
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-02 17:54:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-
 function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!";
+  return `Happy holidays, ${name}!`;
 }
-happyHolidaysTo("you")
+happyHolidaysTo(you)
+
 
 function happyHolidayTo(holiday, name){
   return "Happy ${holiday}, ${name}!";
